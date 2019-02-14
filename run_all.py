89efc4130ea1a0386756b13c70f182f72371ac12
Yuting Li <yutingli@dhcp-172-24-141-165.eduroam.wireless.private.cam.ac.uk>
2019-02-14 13:44:44 +0000

    changes to run_all

--- run_all.py
+++ run_all.py
@@ -1,7 +1,7 @@
 import numpy as np
-from StoEvolution import * 
+from FdEvolution import *
 
-rates = [1e-5, 5e-6, 5e-5, 1e-4, 2.5e-6, 2.5e-5]  
+rates = [6e-6, 7e-6, 8e-6, 9e-6, 1.1e-5]
 
 
 # parameters of the differential equation
@@ -12,35 +12,22 @@ phi_t = 0
 phi_shift = 100
 
 # simulation parameters
-X = 200 
+X = 200
 dx = 0.1
 dt = 1e-3
 n_batches = 100
 
-
-phi_shift = 100 
-phi_target = 0 
+phi_shift = 100
+phi_target = 0
 phi_init = 0
 random = False
 
 
 for u in rates:
 	label = 'X_200_u_{}'.format(u)
-	print(label) 
-	T = 0.5/u 
+	print(label)
+	T = 0.5/u
 	solver = FdEvolution(a, k, u, phi_target, phi_shift)
 	solver.initialise(X, dx, T, dt, n_batches, phi_init, random=random)
 	solver.evolve()
 	solver.save(label)
-
-
-
-
-
-
-
-
-
-
-
-
