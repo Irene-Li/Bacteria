615d56642018147780f5a1d04b89698c62fef338
Yuting Li <yutingli@dhcp-172-24-140-89.eduroam.wireless.private.cam.ac.uk>
2019-03-15 15:02:15 +0000

    Fix previous functions

---
+++ EvolveModelAB.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os.path
+import scipy.sparse as sp
+import scipy.linalg as sl
+import scipy.sparse.linalg
+from scipy.fftpack import fft, ifft, fftfreq
+import json
+from FdEvolution import FdEvolution
