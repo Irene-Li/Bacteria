Fix model AB entropy production calculation

EvolveModelAB.py
@@ -18,23 +18,28 @@ class EvolveModelAB(FdEvolution):
     def load(self, label):
         super().load(label)
         self.M1 = 1
-        self.M2 = self.u * self.phi_shift/2 
+        self.M2 = self.u * self.phi_shift/2
 
     def _modify_params(self):
         # Want dx=1
-		length_ratio = 1/self.dx
-		self.dx = 1
-		self.X = self.X * length_ratio
+        length_ratio = 1/self.dx
+        self.dx = 1
+        self.X = self.X * length_ratio
         self.M1 *= length_ratio**2
         self.k *= length_ratio**2
 
         # Want M1*k= 1
-		time_ratio = length_ratio**4 * (self.k * self.M1)
+        time_ratio = self.M1 * self.k
         self.M1 /= time_ratio
-		self.T = self.T * time_ratio
-		self.M2 /= time_ratio
-		self.dt = self.dt * time_ratio
-		self.step_size = self.step_size * time_ratio
+        self.T = self.T * time_ratio
+        self.M2 /= time_ratio
+        self.dt = self.dt * time_ratio
+        self.step_size = self.step_size * time_ratio
+
+        print(self.k)
+        print(self.M1)
+        print(self.a)
+        print(self.u)
 
     def _make_params_dict(self):
         params = {
@@ -47,37 +52,37 @@ class EvolveModelAB(FdEvolution):
 			'size': self.size,
 			'a': self.a * self.M1,
 			'k': self.k * self.M1,
-			'u': self.u,
+			'u': self.M2 * 2 /self.phi_shift,
 			'phi_shift': self.phi_shift,
 			'phi_target': 0
 		}
-		return params
+        return params
 
-	def rescale_to_standard(self):
-        # Want M1*a = M1*k = 1
-		time_ratio = (self.M1*self.a)**2/self.k
-		space_ratio = np.sqrt(self.a/self.k)
-		self.dx = space_ratio * self.dx
-		self.X = space_ratio * self.X
-		self.T = time_ratio * self.T
-		self.dt = time_ratio * self.dt
-		self.step_size = time_ratio * self.step_size
-		self.M1 = self.M1 * space_ratio**2 / time_ratio
-        self.a
-		self.u = self.u/time_ratio
+    def rescale_to_standard(self):
+        # Want M1 = k = 1
+        space_ratio = np.sqrt(1/self.k)
+        time_ratio = self.k/self.M1
+        self.dx = space_ratio * self.dx
+        self.X = space_ratio * self.X
+        self.T = time_ratio * self.T
+        self.dt = time_ratio * self.dt
+        self.step_size = time_ratio * self.step_size
+        self.M1 = 1
+        self.k = 1
+        self.M2 = self.u/time_ratio
 
     def _fd_delta_phi(self, t, phi):
-        mu1 = self.a * ( - phi + phi ** 3) - self.k * self._laplacian(phi)
+        mu1 =  self.a * ( - phi + phi**3) - self.k * self._laplacian(phi)
         lap_mu1 = self._laplacian(mu1)
-        J2 = self.u * self.phi_shift/2 * self.a * (phi + phi ** 3)
-        delta = lap_mu1 - J2
+        mu2 = self.a * phi**3 + 2 * phi
+        delta = self.M1 * lap_mu1 - self.M2 * mu2
 
         return delta
 
     def _fd_jac(self, t, phi):
         temp = sp.diags([self.a *(3 * phi**2 - 1)], [0], shape=(self.size, self.size))
-        jac = self._laplacian_sparse * temp - self.k * self._laplacian_sparse * self._laplacian_sparse
-        temp_diag = self.u * self.phi_shift * (1 + 3 * phi**2)
+        jac = self.M1 * (self._laplacian_sparse * temp - self.k * self._laplacian_sparse * self._laplacian_sparse)
+        temp_diag = self.M2 * 2 + 3 * self.M2 * self.a * phi**2
         jac -= sp.diags([temp_diag], [0], shape=(self.size, self.size))
         return jac.todense()
 
@@ -93,52 +98,47 @@ class EntropyModelAB(EntropyProductionFourier):
         J_1 = self._gradient_fourier * mu1
         J_1 = ifft(J_1)
 
-        J_2 = self.u * self.phi_shift/2 * self.a *(self.final_phi + self.final_phi**3)
-        M_2 = self.u * self.phi_shift /2
+        J_2 = 2 * self.final_phi + self.a * self.final_phi**3
 
-        self.entropy_from_model_B_current = J_1*J_1
-        self.entropy_from_model_A_current = J_2*J_2/M_2
+        self.entropy_from_model_B_current = J_1*J_1/self.M1
+        self.entropy_from_model_A_current = J_2*J_2/self.M2
         self.entropy = self.entropy_from_model_A_current + self.entropy_from_model_B_current
 
 if __name__ == "__main__":
 
     # Label for the run
-    label = 'X_50_u_1e-6'
+    label = 'X_50_u_8e-4'
 
     # # Parameters
-    # a = 0.2
-    # k = 1
-    # X = 50
-    # dx = 0.1
-    # dt = 1e-3
-    # n_batches = 100
-    # u = 1e-6
-    # T = 1/u
-    # phi_shift = 100
-    # phi_target = 0
-    # phi_init = 0
-    # random = False
+    a = 0.5
+    k = 1
+    X = 50
+    dx = 0.1
+    dt = 1e-3
+    n_batches = 100
+    u = 8e-4
+    T = 10/u
+    phi_shift = 100
+    phi_target = 0
+    phi_init = 0
+    random = False
+
+    start_time = time.time()
+    solver = EvolveModelAB(a, k, u, phi_target, phi_shift)
+    solver.initialise(X, dx, T, dt, n_batches, phi_init, random=random)
+    solver.evolve()
+    solver.save(label)
+    end_time = time.time()
+    print('The simulation took: ')
+    print(end_time - start_time)
+
+    solver.rescale_to_standard()
+    solver.plot_steady_state(label, kink=0)
+
+    # solver = EntropyProductionFourier()
+    # solver.load(label)
     #
-    # start_time = time.time()
-    # solver = EvolveModelAB(a, k, u, phi_target, phi_shift)
-    # solver.initialise(X, dx, T, dt, n_batches, phi_init, random=random)
-    # solver.evolve()
-    # solver.save(label)
-    # end_time = time.time()
-    # print('The simulation took: ')
-    # print(end_time - start_time)
+    # # solver.read_entropy(label)
     #
-    # solver.rescale_to_standard()
-    # solver.plot_steady_state(label, kink=0)
-
-    solver = EntropyProductionFourier()
-    solver.load(label)
-
-    # solver.read_entropy(label)
-
-    # solver.compare_entropy()
     # solver.entropy_with_modelAB_currents()
     # solver.plot_entropy_from_modelAB_currents(label)
-
-    solver.entropy_with_modelAB_currents()
-    solver.plot_entropy_from_modelAB_currents(label)