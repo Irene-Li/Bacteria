ef84722a7e1af4e6b60f4f304f7e401a74e1f103
Yuting Li <yutingli@Yutings-MacBook-Pro.local>
2019-05-20 22:17:55 +0100

    fixes

--- run_sto.py
+++ run_sto.py
@@ -12,26 +12,23 @@ phi_shift = 100
 X = 50
 dx = 0.1
 dt = 1e-3
-T = dt * 1e3
+T = 1
 n_batches = 100
 initial_value = 0
 flat = True
-# n = 5
 
-label = 'sto_dt_{}'.format(u)
+for dt in [1e-3, 1e-4, 1e-5, 1e-6]:
+	label = 'sto_dt_{}'.format(dt)
 
-start_time = time.time()
-solver = StoEvolution(epsilon, a, k, u, phi_t, phi_shift)
-solver.initialise(X, dx, T, dt, n_batches, initial_value, flat=flat)
-solver.save_params(label)
-solver._print_params()
-solver.evolve(verbose=True)
-solver.save_phi(label)
-# solver.evolve_trajectories(n)
-# solver.save_trajs(label)
-end_time = time.time()
-print('The simulation took: {}'.format(end_time - start_time))
-print('Time per trajectory: {}'.format((end_time - start_time)/n))
+	start_time = time.time()
+	solver = StoEvolution(epsilon, a, k, u, phi_t, phi_shift)
+	solver.initialise(X, dx, T, dt, n_batches, initial_value, flat=flat)
+	solver.save_params(label)
+	solver._print_params()
+	solver.evolve(verbose=True)
+	solver.save_phi(label)
+	end_time = time.time()
+	print('The simulation took: {}'.format(end_time - start_time))
 
 
 # evolve trajectories again with different parameters
