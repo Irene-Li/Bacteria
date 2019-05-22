f6771e17443f910def478444a63864c1b2383b60
Irene Li <gnituyil@gmail.com>
2019-05-22 16:10:13 +0100

    Fix typo

--- plot_pattern_length.py
+++ plot_pattern_length.py
@@ -2,11 +2,12 @@ import time
 import numpy as np
 from FdEvolution import *
 
-rates = [3e-5, 1e-5, 3e-6, 1e-6, 3e-7, 1e-7]
-n = np.zeros((3, 2, len(rates)))
+rates = [3e-5, 1e-5, 3e-6, 1e-6, 3e-7]
+Ls = [200, 300, 400]
+n = np.zeros((len(Ls), 2, len(rates)))
 for (i, u)in enumerate(rates):
 	for (j, init) in enumerate(['flat', 'sin']):
-		for (k, length) in enumerate([100, 200, 300]):
+		for (k, length) in enumerate(Ls):
 			label = '{}_X_{}_u_{}'.format(init, length, u)
 			print(label)
 			solver = FdEvolution()
@@ -15,9 +16,10 @@ for (i, u)in enumerate(rates):
 			# solver.plot_steady_state(label, kink=0)
 			# solver.plot_evolution(100, 100, label)
 			n[k, j, i] = solver.count()
+			print(n[k, j, i])
 
-for (k, length) in enumerate([100, 200, 300]):
-	for (j, init) in enumerate(['flat', 'sin']):
+for (k, length) in enumerate(Ls):
+	for (j, init) in enumerate(['sin']):
 		plt.plot(np.log(rates), np.log(length/n[k, j]), 'x', label='{}_X_{}'.format(length, init))
 plt.legend()
 plt.savefig('pattern.pdf')
