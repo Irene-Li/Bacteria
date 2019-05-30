34399613ceb2e132dce07291bdc21a54ebeb5d21
Yuting Li <yutingli@dhcp-172-24-141-193.eduroam.wireless.private.cam.ac.uk>
2019-05-30 14:51:04 +0100

    Droplet init

--- read_sto.py
+++ read_sto.py
@@ -4,11 +4,12 @@ from StoEvolutionPS import *
 labels = ['ep_0_flat_False']
 t_size = 100
 x_size = 200
+u = 1e-5
 
 
-for label in labels:
+for phi_shift in [2]:
+	u = 1e-6/phi_shift
+	label = 'u_{}_phi_s_{}_droplet'.format(u, phi_shift)
 	solver = StoEvolutionPS()
 	solver.load(label)
-	solver.rescale_to_standard()
-	solver.load_phi(label)
 	solver.make_movie(label)
