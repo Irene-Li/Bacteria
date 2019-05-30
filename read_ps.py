ddee20167ac002291192b8a309b947a7593d4faf
Yuting Li <yutingli@dhcp-172-24-141-193.eduroam.wireless.private.cam.ac.uk>
2019-05-30 11:09:44 +0100

    test phi_t

--- read_ps.py
+++ read_ps.py
@@ -2,11 +2,13 @@ import time
 import numpy as np
 from PsEvolution import *
 
-labels = ['u_1e-05_dt_0.001']
+labels = ['u_0_dt_0.0001']
 t_size = 100
 x_size = 200
 
-for label in labels:
-	solver = PsEvolution()
-	solver.load(label)
-	solver.make_movie(label)
+for u in [1e-5, 1e-6]:
+    for dt in [5e-3]:
+        label = 'u_{}_dt_{}'.format(u, dt)
+        solver = PsEvolution()
+        solver.load(label)
+        solver.make_movie(label)
