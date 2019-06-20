fa35d9258ede92632d9b15befa6c39a083888051
Yuting Li <yutingli@Yutings-MacBook-Pro.local>
2019-06-20 01:25:29 +0100

    Add path for saving file

--- continue_run.py
+++ continue_run.py
@@ -2,16 +2,17 @@ import time
 import numpy as np
 from StoEvolutionPS import *
 
-old_label = 'phi_t_-0.7_l=2'
-new_label= 'phi_t_-0.7_l=2_2'
+old_label = 'phi_t_-0.65_u_5e-05_flat_2'
+new_label= 'phi_t_-0.65_u_5e-05_flat_3'
+path ='/store/SOFT/yl511/PS/'
 
-T = 2e4
+T = 1e4
 
 start_time = time.time()
-solver = PsEvolution()
+solver = StoEvolutionPS()
 solver.load(old_label)
 solver.continue_evolution(T)
-solver.save(new_label)
+solver.save(new_label, path=path)
 end_time = time.time()
 print('The simulation took: ')
 print(end_time - start_time)
--- read_sto.py
+++ read_sto.py
@@ -3,11 +3,12 @@ from StoEvolutionPS import *
 
 
 
-for phi_t in [-0.7]:
-# for u in [1e-5, 5e-5]:
-	label = 'phi_t_{}_l=2_X=256'.format(phi_t)
+for phi_t in [-0.6]:
+# for u in [5e-5, 6e-5, 7e-5, 8e-5]:
+	label = 'phi_t_{}_u_8e-05_l=2_3'.format(phi_t)
 	solver = StoEvolutionPS()
 	solver.load(label)
 	solver.print_params()
 	solver.make_movie(label)
+	# solver.make_bd_movie(label)
 	# solver.plot_slices(label)
