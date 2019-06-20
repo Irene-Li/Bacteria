9619f63263de396f433fefd3f20074de6ecf6f4f
Yuting Li <yutingli@dhcp-172-24-141-64.eduroam.wireless.private.cam.ac.uk>
2019-06-20 15:46:33 +0100

    Fix 1D stochastic runs

--- read_sto.py
+++ read_sto.py
@@ -3,9 +3,9 @@ from StoEvolutionPS import *
 
 
 
-for phi_t in [-0.6]:
+for phi_t in [-0.7]:
 # for u in [5e-5, 6e-5, 7e-5, 8e-5]:
-	label = 'phi_t_{}_u_8e-05_l=2_3'.format(phi_t)
+	label = 'phi_t_{}_l=2_X=256_3'.format(phi_t)
 	solver = StoEvolutionPS()
 	solver.load(label)
 	solver.print_params()
