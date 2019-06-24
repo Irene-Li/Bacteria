b7893389f173995129b860e98ae554b3c11e2920
Yuting Li <yutingli@dhcp-172-24-141-252.eduroam.wireless.private.cam.ac.uk>
2019-06-24 14:38:08 +0100

    Change run script

--- run_sto_fd.py
+++ run_sto_fd.py
@@ -4,23 +4,22 @@ from StoEvolution import *
 from multiprocessing import Pool
 
 # parameters of the differential equation
-a = 0.2
+a = 0.05
 k = 1
 epsilon = 0.01
-u = 1e-5
 phi_shift = 10
 phi_target = 0
 
 # simulation parameters
-X = 100
+X = 200
 dx = 1
 dt = 1e-3
-T = 1e5
 n_batches = 100
 flat = True
 initial_value = phi_target
 
 def run(u):
+    T = 5/(u*phi_shift)
     label = 'u_{}_ep_{}'.format(u, epsilon)
     start_time = time.time()
     solver = StoEvolution(epsilon, a, k, u, phi_target, phi_shift)
@@ -30,6 +29,6 @@ def run(u):
     solver.save(label)
     end_time = time.time()
 
-rates = [1e-5, 5e-5, 1e-4]
+rates = [3e-4, 1e-4, 3e-5, 1e-5, 3e-6]
 with Pool(len(rates)) as p:
         print(p.map(run, rates))
