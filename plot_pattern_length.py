fae6c8d0daecff118990c5fb0b7d6a720aa437f2
Yuting Li <yutingli@dhcp-172-24-140-98.eduroam.wireless.private.cam.ac.uk>
2019-07-31 13:36:53 +0100

    Change plotting functions and run scripts

--- plot_pattern_length.py
+++ plot_pattern_length.py
@@ -2,25 +2,23 @@ import time
 import numpy as np
 from FdEvolution import *
 
-rates = [3e-5, 1e-5, 3e-6, 1e-6, 3e-7]
-Ls = [200, 300, 400]
-n = np.zeros((len(Ls), 2, len(rates)))
-for (i, u)in enumerate(rates):
-	for (j, init) in enumerate(['flat', 'sin']):
-		for (k, length) in enumerate(Ls):
-			label = '{}_X_{}_u_{}'.format(init, length, u)
-			print(label)
-			solver = FdEvolution()
-			solver.load(label)
-			solver.rescale_to_standard()
-			# solver.plot_steady_state(label, kink=0)
-			# solver.plot_evolution(100, 100, label)
-			n[k, j, i] = solver.count()
-			print(n[k, j, i])
+deltas = [1000, 3000, 1e4]
+Ls = [400]
+n = np.zeros((len(Ls), len(deltas)))
+for (i, d)in enumerate(deltas):
+	for (k, length) in enumerate(Ls):
+		label = 'X_{}_delta_{}_large'.format(length, d)
+		print(label)
+		solver = FdEvolution()
+		solver.load(label)
+		solver.rescale_to_standard()
+		# solver.plot_steady_state(label)
+		solver.plot_evolution(label, 100, 100)
+		n[k, i] = solver.count()
+		print(n[k, i])
 
 for (k, length) in enumerate(Ls):
-	for (j, init) in enumerate(['sin']):
-		plt.plot(np.log(rates), np.log(length/n[k, j]), 'x', label='{}_X_{}'.format(length, init))
+	plt.plot(np.log(deltas), np.log(length/n[k]), 'x', label='X={}'.format(length))
 plt.legend()
 plt.savefig('pattern.pdf')
 plt.close()
--- run_sto_fd.py
+++ run_sto_fd.py
@@ -4,23 +4,25 @@ from StoEvolution import *
 from multiprocessing import Pool
 
 # parameters of the differential equation
-a = 0.05
+a = 0.1
 k = 1
 epsilon = 0.01
 phi_shift = 10
 phi_target = 0
 
 # simulation parameters
-X = 200
+X = 400
 dx = 1
 dt = 1e-3
 n_batches = 100
 flat = False
 initial_value = phi_target
 
-def run(u):
-    T = 5/(u*phi_shift)
-    label = 'u_{}_ep_{}_sin'.format(u, epsilon)
+def run(delta):
+    u_tilde = alpha_tilde**2/(4*k*(1+delta))
+	u = u_tilde/(phi_target+phi_shift)
+	T = 1e2/(u_tilde*delta)
+    label = 'X_{}_tilde_{}_sin'.format(X, tilde)
     start_time = time.time()
     solver = StoEvolution(epsilon, a, k, u, phi_target, phi_shift)
     solver.initialise(X, dx, T, dt, n_batches, initial_value=initial_value, flat=flat)
@@ -29,6 +31,6 @@ def run(u):
     solver.save(label)
     end_time = time.time()
 
-rates = [3e-4, 1e-4, 3e-5, 1e-5, 3e-6]
+deltas = [1000, 3000]
 with Pool(len(rates)) as p:
         print(p.map(run, rates))
