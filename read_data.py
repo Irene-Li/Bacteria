b73c59591638628c6d2a5bd27ce6c95f2c077675
Yuting Li <yutingli@dhcp-172-24-140-252.eduroam.wireless.private.cam.ac.uk>
2019-08-02 16:23:26 +0100

    change plotting and run scripts

--- read_data.py
+++ read_data.py
@@ -2,11 +2,26 @@ import time
 import numpy as np
 from FdEvolution import *
 
-phi_t = -0.35
-rates = [5e-6]
-for u in rates:
-    label = 'phi_t_{}_u_{}'.format(phi_t, u)
+X = 300
+deltas = [0.04, 0.1]
+
+
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif', size=15)
+for delta in deltas:
+    label = 'X_{}_delta_{}_amp_eq'.format(X, delta)
     solver = FdEvolution()
     solver.load(label)
     # solver.rescale_to_standard()
-    solver.plot_evolution(label, t_size=200, x_size=200)
+    phi = solver.phi[-2]
+    plt.plot(phi, label='$\Delta={}$'.format(delta))
+
+
+plt.xticks([])
+plt.yticks([-1, 0, 1],
+            [r'$\phi_\mathrm{B}$', r'$\phi_\mathrm{t}$', r'$-\phi_\mathrm{B}$'])
+plt.ylim([-1.1, 1.1])
+plt.xlabel(r'$x$')
+plt.legend()
+plt.savefig('amp_eq.pdf')
+plt.close()
--- read_sto.py
+++ read_sto.py
@@ -1,15 +1,19 @@
 import numpy as np
 from StoEvolutionPS import *
 
+delta = 0.2
+X = 128
+phi_t = -0.2
 
-u = 5e-5
-for phi_t in [0.3]:
-# for u in [5e-5, 6e-5, 7e-5, 8e-5]:
-	label = 'phi_t_{}_u_{}_X=256_1'.format(phi_t, u)
-	solver = StoEvolutionPS()
-	solver.load(label)
-	solver.print_params()
-	solver.make_movie(label)
-	# solver.plot_slice(label, n=-1)
-	# solver.make_bd_movie(label)
-	# solver.plot_slices(label)
+label = 'X_{}_delta_{}'.format(X, delta)
+solver = StoEvolutionPS()
+solver.load(label)
+solver.print_params()
+solver.make_movie(label)
+solver.plot_slice(label, n=-1)
+# solver.make_bd_movie(label)
+# solver.plot_slices(label)
+
+phi_average = np.amax(solver.phi, axis=(1,2))
+plt.plot(phi_average)
+plt.show()
