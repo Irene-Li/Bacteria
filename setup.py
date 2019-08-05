f4a2ffecea7f0fc7e1178d0b8968e7c027c33e76
Irene Li <gnituyil@gmail.com>
2019-08-05 21:59:04 +0100

    change setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
 import numpy
-import pyfftw
 
 ext = [Extension("pseudospectral", sources=['pseudospectral.pyx'],
 	 include_dirs=[numpy.get_include()])
