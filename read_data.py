b897ac68f5af3ec82bb733a4e049e4a1c4667f23
Yuting Li <yutingli@Yutings-MacBook-Pro.local>
2019-08-07 15:42:42 +0100

    combine run and read scripts

--- read_data.py
+++ read_data.py
@@ -1,27 +1,23 @@
 import time
 import numpy as np
-from FdEvolution import *
+from TimeEvolution import *
+from StoEvolution2D import *
 
-X = 300
-deltas = [0.04, 0.1]
+twod = True
 
+phi_t = 0
+delta = 0.1
+label = 'det_phi_t_{}_delta_{}'.format(phi_t, delta)
 
-plt.rc('text', usetex=True)
-plt.rc('font', family='serif', size=15)
-for delta in deltas:
-    label = 'X_{}_delta_{}_amp_eq'.format(X, delta)
-    solver = FdEvolution()
-    solver.load(label)
-    # solver.rescale_to_standard()
-    phi = solver.phi[-2]
-    plt.plot(phi, label='$\Delta={}$'.format(delta))
+if twod:
+    solver = StoEvolution2D()
+else:
+    solver = TimeEvolution()
+solver.load(label)
+solver.print_params()
 
-
-plt.xticks([])
-plt.yticks([-1, 0, 1],
-            [r'$\phi_\mathrm{B}$', r'$\phi_\mathrm{t}$', r'$-\phi_\mathrm{B}$'])
-plt.ylim([-1.1, 1.1])
-plt.xlabel(r'$x$')
-plt.legend()
-plt.savefig('amp_eq.pdf')
-plt.close()
+if twod:
+    # solver.make_move(label)
+    solver.plot_slice(label, -1)
+else:
+    solver.plot_evolution(label, t_size=100, x_size=100)
--- run_sto_1d.py
+++ run_sto_1d.py
@@ -6,7 +6,7 @@ from multiprocessing import Pool
 # parameters of the differential equation
 a = 0.1
 k = 1
-epsilon = 0.01
+epsilon = 0.02
 phi_shift = 10
 phi_target = 0
 
@@ -22,8 +22,8 @@ ps = True
 
 
 def run(u):
-    T = 2/u
-    label = 'X_{}_u_{}_flat'.format(X, u)
+    T = 5/u
+    label = 'u_{}_flat'.format(u)
     print(label)
     start_time = time.time()
     solver = StoEvolution1D(epsilon, a, k, u, phi_target, phi_shift)
@@ -31,6 +31,7 @@ def run(u):
     solver.print_params()
     solver.evolve(verbose=True, ps=ps)
     solver.save(label)
+    solver.plot_evolution(label, t_size=200, x_size=200)
     end_time = time.time()
     print('time: {}'.format(end_time - start_time))
 
@@ -38,4 +39,4 @@ def run(u):
 # with Pool(len(us)) as p:
 #     print(p.map(run, us))
 
-run(5e-6)
+run(2e-5)
