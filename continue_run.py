692aa0de446fc4d49efc27dfe451c91756b33612
Yuting Li <yutingli@dhcp-172-24-142-118.eduroam.wireless.private.cam.ac.uk>
2019-08-16 16:35:08 +0100

    add functionality to add perturbations

--- continue_run.py
+++ continue_run.py
@@ -5,11 +5,14 @@ from DetEvolution2D import *
 from StoEvolution1D import *
 from DetEvolution1D import *
 
-sto = False
+sto = True
 twod = True
 
-old_label = 'phi_t_-0.5_u_0.0002_4'
-new_label= 'phi_t_-0.5_u_0.0002_5'
+phi_t = -0.5
+phi_s = 2
+u = 5e-5
+old_label = 'phi_t_{}_phi_s_{}_u_{}_5'.format(phi_t, phi_s, u)
+new_label= 'phi_t_{}_phi_s_{}_u_{}_6'.format(phi_t, phi_s, u)
 
 T = 5e3
 
@@ -25,7 +28,7 @@ else:
     else:
         solver = DetEvolution1D()
 solver.load(old_label)
-solver.continue_evolution(T)
+solver.continue_evolution(T, pert=True)
 solver.save(new_label)
 solver.make_movie(new_label)
 end_time = time.time()
