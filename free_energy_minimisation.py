47dce2eedbcb2a7f03e65550a529f87dc7542c78
Irene Li <gnituyil@gmail.com>
2019-11-28 23:24:33 +0000

    add laminar pattern option to minimisation of free energy

--- free_energy_minimisation.py
+++ free_energy_minimisation.py
@@ -1,138 +1,33 @@
-import numpy as np
-from scipy.special import jv
+from minimise import *
 from matplotlib import pyplot as plt
-
-a = 1
-b = 1
-k = 1
-
-m = 0.01
-c = 0.1
-Nterms = 20
-
-class FreeEnergy:
-
-    def __init__(self, a, b, k, c, m):
-        self.a = a
-        self.b = b
-        self.k = k
-        self.c = c
-        self.m = m
-        self.a_eff = self.a + self.k*self.m**2
-        self.sigma = np.sqrt(self.a*self.k/18)
-
-    def initialise(self, Nterms):
-        pass
-
-    def free_energy_for_uniform(self):
-        phi_t = - (self.c/self.b)**(1/3)
-        f = self._f(phi_t)
-        f_nl = self.a_eff*phi_t**2/2
-        return f + f_nl
-
-    def free_energy_nl(self, L, A, B, ratio):
-        chi = self.m*L*self.lattice_scale_factor/(2*np.pi)
-        nx = np.arange(1, self.Nterms)
-        ny = np.arange(0, self.Nterms)
-        nx, ny = np.meshgrid(nx, ny)
-        n = self._distance(nx, ny)
-        Ek = self._Ek(n, ratio, chi)
-        Ek[n>Nterms*self.lattice_scale_factor]=0
-        sum = np.sum(Ek)
-        term1 = self.lattice_sym*(chi*ratio)**2*sum
-        area_ratio = np.pi/self.lattice_scale_factor*ratio**2
-        term2 = (A*area_ratio+B*(1-area_ratio))**2
-        return self.a_eff*(term1+term2)/2
-
-    def free_energy_local(self, L, A, B, ratio):
-        area_ratio = np.pi/self.lattice_scale_factor*ratio**2
-        term1 = self._f(A)*area_ratio + self._f(B)*(1-area_ratio)
-        term1 += self.sigma*(A-B)**2*2*np.pi/self.lattice_scale_factor*ratio/L
-        return term1
-
-    def total_free_energy_for_range(self, Lmax, deltaL, A, B):
-        Ls = np.arange(1, Lmax, deltaL)
-        free_energy = np.empty_like(Ls, dtype='float64')
-        r = self._ratio(A, B)
-        for (i, L) in enumerate(Ls):
-            free_energy[i] = self.free_energy_local(L, A, B, r)+self.free_energy_nl(L, A, B, r)
-        return free_energy
-
-    def min_free_energy(self, Lmax, A, B):
-        r = self._ratio(A, B)
-        minimum = 0
-        for L in np.arange(1, Lmax, 1):
-            f = self.free_energy_local(L, A, B, r)
-            f+= self.free_energy_nl(L, A, B, r)
-            minimum = min(f, minimum)
-        return minimum
-
-    def _f(self, x):
-        return self.c*x - self.a*x**2/2 + self.b*x**4/4
-
-    def _Ek(self, n, ratio, chi):
-        term1 = jv(1, ratio*n*2*np.pi/self.lattice_scale_factor)/n
-        term2 = chi**2 + n**2
-        return term1**2/term2
-
-    def _ratio(self, A, B):
-        pass #to implement in subclasses
-
-    def _distance(self, nx, ny):
-        pass #to impletement in subclass
+import time
 
 
 
-class FreeEnergySquare(FreeEnergy):
-
-    def initialise(self, Nterms):
-        self.Nterms = Nterms
-        self.lattice_scale_factor = 1
-        self.lattice_sym = 4
-
-    def _ratio(self, A, B):
-        return np.sqrt((- self.c - self.b*B**3)/(A**3 - self.b*B**3)/np.pi)
-
-    def _distance(self, nx, ny):
-        return np.sqrt(nx**2+ny**2)
-
-
-class FreeEnergyTriangle(FreeEnergy):
-
-    def initialise(self, Nterms):
-        self.Nterms = Nterms
-        self.lattice_scale_factor = np.sqrt(3)/2
-        self.lattice_sym = 6
-
-    def _ratio(self, A, B):
-        return np.sqrt(np.sqrt(3)*(-self.c - self.b*B**3)/(2*np.pi*self.b*(A**3-B**3)))
-
-    def _distance(self, nx, ny):
-        return np.sqrt(nx**2+ny**2+nx*ny)
-
-
-def plot_free_energy(objs, A, B, Lmax, deltaL):
+def plot_free_energy(objs, A, B, Lmax):
     legends = ['square lattice', 'hexagonal lattice']
-    Ls = np.arange(1, Lmax, deltaL)
+    Ls = np.linspace(1, Lmax, 1000)
     plt.rc('text', usetex=True)
     plt.rc('font', family='serif', size=16)
 
     for (i, obj) in enumerate(objs):
-        free_energy = obj.total_free_energy_for_range(Lmax, deltaL, A, B)
+        free_energy = obj.total_free_energy_for_range(Lmax, A, B)
         plt.plot(Ls, free_energy, label=legends[i])
-
-    plt.axhline(y=objs[0].free_energy_for_uniform(), linestyle='--', label='uniform state')
-
+    f_uni = objs[0].free_energy_for_uniform()
+    f_laminar = objs[0].free_energy_for_laminar_range(Lmax, A, B)
+    plt.axhline(y=f_uni, linestyle='--', label=r'uniform state')
+    plt.plot(Ls, f_laminar, label=r'laminar')
     plt.legend()
-    plt.ylim([1.1*min(free_energy), -min(free_energy)*0.5])
+    plt.ylim([1.1*min(free_energy), 2*f_uni - min(free_energy)])
     plt.ylabel(r'$V^{-1}\mathcal{F}$')
     plt.xlim([0, Lmax])
     plt.xlabel(r'$L$')
     plt.tight_layout()
-    plt.savefig('total_free_energy.pdf')
+    plt.show()
+    # plt.savefig('total_free_energy.pdf')
     plt.close()
 
-def minimise(obj, Lmax):
+def plot_minimise(obj, Lmax):
     step_size = 0.05
     low = 0.6
     high = 1.8
@@ -143,6 +38,7 @@ def minimise(obj, Lmax):
     for (i, A) in enumerate(As):
         for (j, B) in enumerate(Bs):
             min_free_energy[j, i] = obj.min_free_energy(Lmax, A, B)
+
     plt.rc('text', usetex=True)
     plt.rc('font', family='serif', size=16)
 
@@ -159,19 +55,39 @@ def minimise(obj, Lmax):
     plt.savefig('min_free_energy.pdf')
     plt.close()
 
-
+def phase_diagram(obj, Lmax):
+    logms = np.arange(-10, -2, 2)
+    ms = np.exp(logms)
+    phits = np.arange(-1, 0, 0.2)
+    phases = np.empty((len(phits), len(ms)), dtype='int')
+    for (i, m) in enumerate(ms):
+        for (j, phit) in enumerate(phits):
+            obj.set_m_phit(m, phit)
+            print(m, phit)
+            Lmax = 10/m
+            lat_min, lam_min = obj.minimise(Lmax, phit)
+            uniform = obj.free_energy_for_uniform()
+            phases[j, i] = np.argmin([uniform, lat_min, lam_min])
+    # plt.pcolor(logms, phits, phases)
+    # plt.show()
 
 if __name__ == '__main__':
     a = 0.2
     b = 0.2
     k = 1
-    c = 0.04
-    m = 0.01
+    c = 0.0
+    m = np.sqrt(1e-4)
     Nterms = 20
+
+    print(np.sqrt(2*k/a))
 
     sq_obj = FreeEnergySquare(a, b, k, c, m)
     tri_obj = FreeEnergyTriangle(a, b, k, c, m)
     sq_obj.initialise(Nterms)
     tri_obj.initialise(Nterms)
-    # plot_free_energy([sq_obj, tri_obj], 1, -1, 1000, 1)
-    minimise(tri_obj, 1000)
+    # plot_free_energy([sq_obj, tri_obj], 1, -1, 1000)
+
+    start_time = time.time()
+    phase_diagram(tri_obj, 800)
+    end_time = time.time()
+    print(end_time - start_time)
--- plot_patterns.py
+++ plot_patterns.py
@@ -3,11 +3,20 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import AxesGrid
 from StoEvolution2D import *
 
-slices = [10, 30, -1]
-phi_t = 0
-delta = 0.1
-epsilon = 1e-4
-add_ons = ['', '', '_2']
+# plot amp eq
+# slices = [10, 40, -1]
+# phi_t = 0
+# delta = 0.1
+# epsilon = 1e-4
+# # add_ons = ['', '', '_2']
+
+
+# plot droplet splitting patterns
+labels = ['StoPs2d/droplet_splitting/phi_t_-0.6_u_4e-05_X=256_12',
+'StoPs2d/droplet_splitting/phi_t_-0.6_u_4e-05_X=256_14',
+'StoPs2d/droplet_splitting/phi_t_-0.6_u_4e-05_X=256_17',
+'2DLimitCycles/phi_t_-0.3_phi_s_1.5_u_8e-05_4']
+slices = [10, -1, 10, -1]
 
 ncols = len(slices)
 
@@ -23,14 +32,15 @@ grid = AxesGrid(fig, 111, nrows_ncols=(1, ncols),
 for i in range(ncols):
     s = slices[i]
     ax = grid[i]
-    label = 'phi_t_{}_delta_{}_epsilon_{}{}'.format(phi_t, delta, epsilon, add_ons[i])
+    # label = 'phi_t_{}_delta_{}_epsilon_{}{}'.format(phi_t, delta, epsilon, add_ons[i])
+    label = labels[i]
     solver = StoEvolution2D()
     solver.load(label)
     phi = solver.phi[s]
-    # if i < 3:
-    #     phi = np.roll(phi, 100, axis=-1)
-    #     phi = (phi[::2, ::2] + phi[1::2, 0::2] + phi[0::2, 1::2] + phi[1::2, 1::2])/4
-    #     print(phi.shape)
+    if i < 3:
+        phi = np.roll(phi, 100, axis=-1)
+        phi = (phi[::2, ::2] + phi[1::2, 0::2] + phi[0::2, 1::2] + phi[1::2, 1::2])/4
+        print(phi.shape)
     ax.set_axis_off()
     im = ax.imshow(phi, vmin=-1, vmax=1, cmap='seismic', interpolation='none')
 
@@ -39,5 +49,5 @@ cbar = grid.cbar_axes[0].colorbar(im)
 cbar.ax.set_yticks([-1, 0, 1])
 cbar.ax.set_yticklabels([r'-$\phi_\mathrm{B}$', r'0', r'$\phi_\mathrm{B}$'])
 
-plt.savefig('amp_eq_patterns.pdf')
+plt.savefig('dumbbell.pdf')
 plt.close()
